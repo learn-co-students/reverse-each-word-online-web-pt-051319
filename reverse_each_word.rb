7c822023fb84c2d3b27185a66979defed53fe34f
James Gomez <james.gomez320@yahoo.com>
2019-05-23 05:57:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,21 @@
-reverse_each_word_with_each(string)
-seperates_words = string.split("")
-array =[]
-seperates_words.each do |string|
-  array << string.reverse
+def reverse_each_word(string)
+original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
-return array.join("")
 
-reverse_each_word_with_collect(string)
-seperates_words_one = string.split("")
-new_array = []
-seperates_words_one.collect do |string|
-  new_array << string.reverse
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
-new_array.join("")
+
 
 
   
